7b28c2e46045212352d62519772dc0e744b615a2
Sebastian Pozoga <sebastian@pozoga.eu>
2017-06-20 21:34:42 +0200

    add repositories service mockup

---
+++ cliapp/common/mockups/repositories.go
@@ -0,0 +1,30 @@
+package mockups
+
+import (
+	"fmt"
+
+	"github.com/goatcms/goatcli/cliapp/services"
+	"github.com/goatcms/goatcore/filesystem"
+)
+
+// RepositoriesService is a simple mockup for  services.Repositories
+type RepositoriesService struct {
+	data map[string]filesystem.Filespace
+}
+
+// NewRepositoriesService create mockup for services.Repositories
+func NewRepositoriesService(data map[string]filesystem.Filespace) services.Repositories {
+	return RepositoriesService{
+		data: data,
+	}
+}
+
+// Filespace return filespace for repository and revision
+func (r RepositoriesService) Filespace(repository, rev string) (filesystem.Filespace, error) {
+	key := repository + "!" + rev
+	fs, ok := r.data[key]
+	if !ok {
+		return nil, fmt.Errorf("key not exist %s", key)
+	}
+	return fs, nil
+}
